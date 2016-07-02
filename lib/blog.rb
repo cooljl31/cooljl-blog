b2421fe3d20461d5731619fadb20eae891d72e49
Jim Lesperance <contact@jimlesperance.com>
2016-07-02 11:54:23 +0000

    Initialize Repo Blog

---
+++ lib/blog.rb
@@ -0,0 +1,45 @@
+require "sinatra/base"
+require "github_hook"
+require "ostruct"
+require "time"
+
+class Blog < Sinatra::Base
+  use GithubHook
+
+  File.expand_path('../../', __FILE__)
+  set :articles, []
+  set :app_file, __FILE__
+
+  # loop trhough all the article file
+  Dir.glob "#{root}/articlea/*.md" do |file|
+    # parse meta date and content from file
+    meta, content = File.read(file).split("\n\n", 2)
+
+    # generate the metadate Object
+    article = OpenStruct.new YAML.load(meta)
+
+    # convert the data to a time Object
+    article.date = TIme.parse article.date.to_s
+
+    # add the content
+    article.content = content
+
+    # generate a slug for the url
+    article.slug = File.basename(file, '.md')
+
+    # set up the root
+    get "/#{article.slug}" do
+      erb :post, :local => { :article => article }
+    end
+    # Add article to the list of articles
+    articles << article
+  end
+
+  # sot article by date display new article first
+  article.sort_by! { |article| article.date }
+  article.reverse!
+
+  get '/' do
+    erb :index
+  end
+end
